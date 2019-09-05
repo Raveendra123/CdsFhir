e24d301ae64cfe1e28b387b1ff7de7b2d04675f5
Raveendra Babu Pagadala <pagadala.ravindra@gmail.com>
2019-09-05 15:53:16 +0530

    new project for cdsfhir

---
+++ src/Microsoft.Health.DynamicsCrm/DynamicsCrmFhirDataStore.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Health.DynamicsCrm;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Health.Fhir.DynamicsCrm
+{
+    public class DynamicsCrmFhirDataStore
+    {
+        public JObject GetCdsObservationData()
+        {
+            ClientCredential credential = new ClientCredential("fe4c5bad-24a7-433a-a523-318a435ad676", "n3eIliUcxnEzRLu**G0Z3n.@xCys6kKC");
+            string authorityUri = "https://login.microsoftonline.com/2c55b088-a14f-4c9e-8a12-c13454aa56dd/oauth2/authorize";
+
+            AuthenticationContext context = new AuthenticationContext(authorityUri);
+            var result = context.AcquireTokenAsync("https://cggtech.crm.dynamics.com", credential);
+
+            var authToken = result.Result.AccessToken;
+
+            JObject observationData = null;
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+            httpClient.DefaultRequestHeaders.Add("OData-Version", "4.0");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+
+            var response = httpClient.GetAsync("https://cggtech.crm.dynamics.com/api/data/v9.1/msemr_observations?$select=msemr_observationid,msemr_description,createdon&$top=20").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                //// Get the response content and parse it.
+                observationData = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return observationData;
+        }
+    }
+}
---
+++ src/Microsoft.Health.DynamicsCrm/OAuthMessageHandler.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Microsoft.Health.DynamicsCrm
+{
+    internal class OAuthMessageHandler : DelegatingHandler
+    {
+        private AuthenticationHeaderValue authHeader;
+
+        public OAuthMessageHandler(string serviceUrl, string clientId, string username, string password, HttpMessageHandler innerHandler)
+
+            : base(innerHandler)
+        {
+            // Obtain the Azure Active Directory Authentication Library (ADAL) authentication context.
+
+            AuthenticationParameters ap = AuthenticationParameters.CreateFromResourceUrlAsync(new Uri(serviceUrl + "/api/data/v9.1/")).Result;
+
+            AuthenticationContext authContext = new AuthenticationContext(ap.Authority, false);
+
+            ClientCredential credential = new ClientCredential("d2bff45d-5dba-4f19-8841-d8d979eb6dab", "a+Ly3st-Z6pCmxVhodf/pCLTRHD2Ax?9");
+
+            string authorityUri = "https://login.microsoftonline.com/c1159de4-f0a1-46e0-ad9a-b55bc7c24880/oauth2/authorize";
+
+            AuthenticationContext context = new AuthenticationContext(authorityUri);
+            var authResult = context.AcquireTokenAsync("https://ryantest12345.crm.dynamics.com", credential);
+
+            authHeader = new AuthenticationHeaderValue("Bearer", authResult.Result.AccessToken);
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = authHeader;
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
